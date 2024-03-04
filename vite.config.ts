013f27d56101881872eaf6f501a37a375a033931
Klas Mellbourn <klas@mellbourn.net>
2024-03-04 14:01:25 +0100

    tried to make dynamic extensions, failed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import alias from "@rollup/plugin-alias";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        { find: "constants.customer1.ts", replacement: "constants.ts" },
+        { find: /^(.*)\.customer1.ts$/, replacement: "$1.ts" },
+        { find: /constants.customer1.ts$/, replacement: "constants.ts" },
+      ],
+    }),
+  ],
+});
