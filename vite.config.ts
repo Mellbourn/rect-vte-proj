fa07276a463e7b8eb146e0c05db560adc9070868
Klas Mellbourn <klas@mellbourn.net>
2024-03-08 14:48:02 +0100

    reorder extensions in vite.config.ts too

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: [".customer1.ts", ".ts", ".tsx"],
+  },
+});
+console.log("config", config);
+// https://vitejs.dev/config/
+export default config;
